94f39901b578850a657ff23448c1483e671d1e9e
Max <maxwelljblanco@gmail.com>
2025-09-12 08:11:41 -0500

    started flatlistnotes

--- FlatList_L8_L9/app/index.tsx
+++ FlatList_L8_L9/app/index.tsx
@@ -7,15 +7,54 @@ import {
 } from "react-native";
 import colors from "@/styles/colors";
 import defaultStyles from "@/styles/defaultStyles";
+import { useState } from "react";
 
 export default function Index() {
+
+  type dataType = {
+    id: string; // refer to the unique identifier
+    title: string; // text we'll show in the list
+  }
+
+  // using all caps cause data will never change during use
+  const DATA: dataType[] = [
+    {id: "1", title: "First Item"},
+    {id: "2", title: "Second Item"},
+    {id: "3", title: "Third Item"},
+    {id: "4", title: "Fourth Item"},
+  ];
+
+  // create a simple function telling me what was selective
+  const selectedList = (item: dataType) => {
+    console.log(item.title);
+    setSelectedId(item.id);
+  };
+
+  const [selectedId, setSelectedId] = useState<string>("1");
+
   return (
     <View style={defaultStyles.container}>
       <View style={defaultStyles.titleContainer}>
         <Text style={defaultStyles.title}>Insert Title Here</Text>
       </View>
       <View style={[defaultStyles.textContainer, { flex: 1 }]}>
-        <View style={styles.flatlist}>
+        <View style={styles.flatlistRow}>
+          <FlatList
+            data = {DATA}
+            keyExtractor = {(item: dataType) => item.id}
+            renderItem = {({  item }) => (
+              <TouchableOpacity onPress = {() => selectedList(item)}>
+                <View style={styles.flatlist}>
+                  <Text>
+                    {item.title}
+                  </Text>
+                </View>
+
+              </TouchableOpacity>
+            )}
+
+
+            />
           <Text>This is where our list will go</Text>
         </View>
       </View>
@@ -26,6 +65,10 @@ export default function Index() {
 const styles = StyleSheet.create({
   flatlist: {
     alignItems: "center",
+  },
+  flatlistRow: {
+    alignItems: "center",
+    backgroundColor: colors.secondary,
   },
   titleContainer: {
     marginTop: 5,
